86184d1ecd532633c6c3785700dac6ea328b8723
Павел <pavel.tagiltsef@gmail.com>
2022-02-01 20:04:25 +0300

    Updates

---
+++ precache-manifest.4f8f280456264ab3980efa6194717237.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ecd6f76b05e964057756",
+    "url": "/react-got-app/static/js/main.ecd6f76b.chunk.js"
+  },
+  {
+    "revision": "eb8aa6728def6a055b59",
+    "url": "/react-got-app/static/css/1.2cc56178.chunk.css"
+  },
+  {
+    "revision": "eb8aa6728def6a055b59",
+    "url": "/react-got-app/static/js/1.eb8aa672.chunk.js"
+  },
+  {
+    "revision": "efbfe6faf7be6d416ceb",
+    "url": "/react-got-app/static/js/runtime~main.efbfe6fa.js"
+  },
+  {
+    "revision": "5b89843d49f78fbed8dd2df09d32e7b7",
+    "url": "/react-got-app/static/media/error.5b89843d.jpg"
+  },
+  {
+    "revision": "6b64b12c468df782af34d575c29af2c8",
+    "url": "/react-got-app/static/media/background.6b64b12c.jpg"
+  },
+  {
+    "revision": "13d16ae43fcb39ec3a964d82e51704d6",
+    "url": "/react-got-app/static/media/houses.13d16ae4.jpg"
+  },
+  {
+    "revision": "4cd390b7057b6612f023484604f3abc4",
+    "url": "/react-got-app/index.html"
+  }
+];
